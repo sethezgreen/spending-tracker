7640dd6716a587e9f8148b0bbcce4edc4a0c7caa
sethezgreen <sethezgreen@gmail.com>
2024-02-07 20:45:33 -0500

    added update and delete functionality for Month model

--- server/controllers/month.controller.js
+++ server/controllers/month.controller.js
@@ -21,5 +21,38 @@ module.exports = {
             .catch((err) => {
                 res.status(500).json(err)
             })
+    },
+
+    // Read One Month
+    findMonth: (req, res) => {
+        Month.findOne({_id: req.params.id})
+            .then((month) => {
+                res.status(200).json(month)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    // Update Month
+    updateMonth: (req, res) => {
+        Month.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+            .then((updatedMonth) => {
+                res.json(updatedMonth)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    // Delete Month
+    deleteMonth: (req, res) => {
+        Month.deleteOne({_id: req.params.id})
+            .then((result) => {
+                res.status(201).json(result)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
     }
 }
--- server/routes/month.routes.js
+++ server/routes/month.routes.js
@@ -3,4 +3,10 @@ const MonthController = require('../controllers/month.controller')
 module.exports = (app) => {
     app.post('/api/create', MonthController.createMonth)
     app.get('/api/months', MonthController.findAllMonths)
+    app.get('/api/month/:id', MonthController.findMonth)
+    app.put('/api/updateMonth/:id', MonthController.updateMonth)
+    app.delete('/api/deleteMonth/:id', MonthController.deleteMonth)
+
+    // Expense routes
+    // app.put('/api/month/:monthId/expenses/:expenseId', MonthController.updateExpense)
 }
