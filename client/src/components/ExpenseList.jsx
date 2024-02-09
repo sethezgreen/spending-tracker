b7107e7550702f164eae72a690fa2cffd1e8a2c5
sethezgreen <sethezgreen@gmail.com>
2024-02-08 22:20:53 -0500

    added ExpenseList component

---
+++ client/src/components/ExpenseList.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ExpenseList = (props) => {
+    const [expenses, setExpenses] = useState([])
+    const {id} = props
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/month/${id}`)
+            .then((res) => {
+                console.log(res.data.expenses)
+                setExpenses(res.data.expenses)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+    return (
+        <div>
+            {
+                expenses.map((expense) => (
+                    <div key={expense._id}>
+                        <p>{expense.expenseName}</p>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default ExpenseList
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import ExpenseList from '../components/ExpenseList'
 
 const Main = (props) => {
     const [months, setMonths] = useState([])
+    const [monthId, setMonthId] = useState("65c58ba8d1a7b497ff69d233")
 
     return (
         <div>
             <h1>Spending Tracker</h1>
             <Link to={`/month/create`}>Add Month</Link>
+            <p>Expenses</p>
+            <ExpenseList id={monthId}/>
         </div>
     )
 }
