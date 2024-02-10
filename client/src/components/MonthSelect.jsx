5137a8e39134314a7b116d67ec046635e5c63972
Seth Green <sethezgreen@gmail.com>
2024-02-10 14:33:10 -0500

    created MonthSelect component

---
+++ client/src/components/MonthSelect.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const MonthSelect = (props) => {
+    const {setMonthId} = props
+    const [months, setMonths] = useState([])
+    const [dropdown, setDropdown] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/months')
+            .then((res) => {
+                console.log(res)
+                setMonths(res.data)
+                generateDropdown(res.data)
+            })
+            .catch(err => console.log(err))
+        },[])
+        
+    const generateDropdown = (months) => {
+        const options = [months.map((month) => {
+            return (
+                <option value={month._id} >{month.month}</option>
+            )
+        })]
+        console.log(options)
+        setDropdown(options)
+    }
+
+    return (
+        <select onChange={(e) => setMonthId(e.target.value)}>
+            {
+                dropdown.map((option) => (
+                    option
+                ))
+            }
+        </select>
+    )
+}
+
+export default MonthSelect
--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -3,29 +3,17 @@ import { Link } from 'react-router-dom'
 import ExpenseList from '../components/ExpenseList'
 import ExpenseForm from '../components/ExpenseForm'
 import axios from 'axios'
+import MonthSelect from '../components/MonthSelect'
 
 const Main = (props) => {
     const [months, setMonths] = useState([])
     const [monthId, setMonthId] = useState("65c6eaa6875fd16dafd8b7c7")
     const [expenses, setExpenses] = useState([])
 
-    useEffect(() => {
-        axios.get('http://localhost:8000/api/months')
-            .then((res) => {
-                setMonths(res.data)
-            })
-            .catch(err => console.log(err))
-    },[])
-
     return (
         <div>
             <h1>Spending Tracker</h1>
-            <p>months</p>
-            {
-                months.map((month) =>(
-                    <p key={month._id}>{month.month}</p>
-                ))
-            }
+            <MonthSelect setMonthId={setMonthId}/>
             <Link to={`/month/create`}>Add Month</Link>
             <p>Expenses</p>
             <ExpenseForm monthId={monthId} expenses={expenses} setExpenses={setExpenses}/>
