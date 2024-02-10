12eb709381e2ef245a870b39e35e658e3ee1f078
Seth Green <sethezgreen@gmail.com>
2024-02-10 17:49:09 -0500

    added delete functionality

--- client/src/components/ExpenseList.jsx
+++ client/src/components/ExpenseList.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import '../App.css'
 
 const ExpenseList = (props) => {
-    const {expenses, setExpenses, id} = props
+    const {expenses, setExpenses, monthId} = props
 
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/month/${id}`)
+        axios.get(`http://localhost:8000/api/month/${monthId}`)
             .then((res) => {
                 // console.log(res.data.expenses)
                 setExpenses(res.data.expenses)
@@ -14,7 +14,15 @@ const ExpenseList = (props) => {
             .catch((err) => {
                 console.log(err)
             })
-    }, [id])
+    }, [monthId])
+
+    const deleteHandler = (expenseId) => {
+        axios.put(`http://localhost:8000/api/month/${monthId}/expense/delete/${expenseId}`)
+            .then(() => {
+                setExpenses(expenses.filter(expense => expense._id !== expenseId))
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <div>
@@ -25,6 +33,7 @@ const ExpenseList = (props) => {
                             <tr key={expense._id}>
                                 <td>{expense.expenseName}</td>
                                 <td>${expense.price}</td>
+                                <td><button onClick={(e) => deleteHandler(expense._id)}>delete</button></td>
                             </tr>
                     ))
                 }
